66471461d9aab8a5dd626f3254403bd956f1a1a3
diniscarvalho19 <49685992+diniscarvalho19@users.noreply.github.com>
2019-12-13 00:04:11 +0000

    Pushings

--- src/main/java/com/mycompany/projetopoo/CISUC.java
+++ src/main/java/com/mycompany/projetopoo/CISUC.java
@@ -435,6 +435,28 @@ public String getInfo(Pessoa p){
     }    
     return info;
 }
+
+public String getInfoProjetos(Projeto p){
+    String info;
+    if(p.getCompleto() == true){
+        info = String.format("Nome: %s\nAcrónimo: %s\nData de Inicio: %d-%d-%d\nDuração: %d\nData Final: %d-%d-%d\n",p.getNome(),p.getAcronimo(),p.getData_inicio().get(GregorianCalendar.DAY_OF_MONTH),p.getData_inicio().get(GregorianCalendar.MONTH)+1,p.getData_inicio().get(GregorianCalendar.YEAR),p.getData_final().get(GregorianCalendar.DAY_OF_MONTH),p.getData_final().get(GregorianCalendar.MONTH)+1,p.getData_final().get(GregorianCalendar.YEAR),p.getDuracao());
+    }
+    else{
+        info = String.format("Nome: %s\nAcrónimo: %s\nData de Inicio: %d-%d-%d\nDuração: %d\n",p.getNome(),p.getAcronimo(),p.getData_inicio().get(GregorianCalendar.DAY_OF_MONTH),p.getData_inicio().get(GregorianCalendar.MONTH)+1,p.getData_inicio().get(GregorianCalendar.YEAR),p.getDuracao());
+    }    
+    return info;
+}
+
+//public String getInfoTarefas(Tarefa p){
+//    String info;
+//    if(p.getTaxa() == 1){
+//        info = String.format("Nome: %s\nEstatuto: %s\nEmail: %s\nInicio da Bolsa: %d-%d-%d\nFim da Bolsa: %d-%d-%d\nCusto: %d",p.getNome(),p.getEstatuto(),p.getEmail(),p.getInicio().get(GregorianCalendar.DAY_OF_MONTH),p.getInicio().get(GregorianCalendar.MONTH)+1,p.getInicio().get(GregorianCalendar.YEAR),p.getFim().get(GregorianCalendar.DAY_OF_MONTH),p.getFim().get(GregorianCalendar.MONTH)+1,p.getFim().get(GregorianCalendar.YEAR),p.getCusto());
+//    }
+//    else{
+//        info = String.format("Nome: %s\nEstatuto: %s\nEmail: %s\nNúmero Mecanográfico: %d\nÁrea de investigação: %s\n",p.getNome(),p.getEstatuto(),p.getEmail(),p.getNumM(),p.getArea());
+//    }    
+//    return info;
+//}
     
 
 public int leObjectFilesBolseiros(){
